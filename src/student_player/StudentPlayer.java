8717f535ee7c369caf1432984938c22d89b265d1
Marc Yang <m.y.yang001@gmail.com>
2016-04-01 10:45:12 -0400

    think two steps ahead for attack

--- src/student_player/StudentPlayer.java
+++ src/student_player/StudentPlayer.java
@@ -30,7 +30,7 @@ public class StudentPlayer extends HusPlayer {
         int[][] pits = board_state.getPits();
 
         // Use ``player_id`` and ``opponent_id`` to get my pits and opponent pits.
-
+        final HusBoardState f_board_state = board_state;
         final int[] my_pits = pits[player_id];
         final int[] op_pits = pits[opponent_id];
         //the number associated with my_pit[0] is the number of rocks in that specific hole
@@ -57,20 +57,32 @@ public class StudentPlayer extends HusPlayer {
 
                 class Pot_Gain implements Runnable {
 
-                    private volatile PotentialOutCome pg;
-
+                    //private volatile ArrayList<PotentialOutCome> pg_list = new ArrayList<PotentialOutCome>();
+                    private volatile PotentialOutCome second_pg;
                     @Override
                     public void run() {
 
                         //get the list of largest enemy columns
                         List<PotentialOutCome> tm = MyTools.ColumnWithLargestSum(op_pits, MyTools.Outcome.GAIN);
-                        //find the potential gain
-                        pg = MyTools.potentialOutCome(tm, my_pits, MyTools.Outcome.GAIN);
+                        ArrayList<PotentialOutCome> potentialMoves = MyTools.potentialMoves(tm, my_pits, MyTools.Outcome.GAIN);
+                        // look for two steps ahead
+                        if (potentialMoves.size() > 0) {
+                            for (int i = 0; i < potentialMoves.size(); i++) {
+                                //find the potential gain
+                                HusMove p_move = new HusMove(potentialMoves.get(i).pitToMove, player_id);
+                                HusBoardState cloned_board_state = (HusBoardState) f_board_state.clone();
+                                cloned_board_state.move(p_move);
+                                List<PotentialOutCome> second_steps = MyTools.ColumnWithLargestSum(cloned_board_state.getPits()[opponent_id], MyTools.Outcome.GAIN);
+                                second_pg = MyTools.potentialOutCome(second_steps, cloned_board_state.getPits()[player_id], MyTools.Outcome.GAIN);
+
+                            }
+                        }
+
                     }
 
                     public PotentialOutCome getPg() {
                         System.out.println("calling another thread");
-                        return pg;
+                        return second_pg;
                     }
                 }
 
@@ -93,9 +105,7 @@ public class StudentPlayer extends HusPlayer {
 
                 PotentialOutCome pg = t_gain.getPg();
 
-                if (pg != null) {
-                    System.out.println("Potential Gain: " + pg.rocks);
-                }
+
 
                 if (pl != null) {
                     System.out.println("Potential Loss: " + pl.rocks);
@@ -107,7 +117,9 @@ public class StudentPlayer extends HusPlayer {
                     if (pg.rocks >= pl.rocks) {
                         //attack/ capture
                         move = new HusMove(pg.pitToMove, player_id);
+
                         System.out.println("Attack TURN NUMBER: " + board_state.getTurnNumber() + "Pit #: " + pg.pitToMove);
+
 
                     } else {
                         //when potential loss > potential gain
@@ -132,6 +144,15 @@ public class StudentPlayer extends HusPlayer {
                     //no gain no loss
                     System.out.println("Random Move");
                     move = moves.get(MyTools.randomLegalMove(moves.size()));
+                    //plan two steps ahead
+
+                    // We can see the effects of a move like this...
+                    //HusBoardState cloned_board_state = (HusBoardState) board_state.clone();
+                    //cloned_board_state.move(move);
+
+
+
+
                 }
                 //check legal move
                 if (board_state.isLegal(move)) {
@@ -147,9 +168,7 @@ public class StudentPlayer extends HusPlayer {
         // But since this is a placeholder algorithm, we won't act on that information.
         return move;
     }
-    // We can see the effects of a move like this...
-    //HusBoardState cloned_board_state = (HusBoardState) board_state.clone();
-    //cloned_board_state.move(move);
+
 
 
 }
--- src/student_player/mytools/MyTools.java
+++ src/student_player/mytools/MyTools.java
@@ -147,12 +147,102 @@ public class MyTools extends StudentPlayer{
             return queue.get(0);
         }
 
-
         //return the potential loss
         return PotentialOutCome;
 
     }
 
+
+    /**
+     *
+     * @param largestPits => sorted list of enemy pits that contain the largest sum
+     * @param the_pits => my current pits
+     * @param result => type gain / loss
+     * @return gives the Potential_Attack, which contains which pit to move and its potential gain.
+     */
+
+
+    public static ArrayList<PotentialOutCome> potentialMoves(List<PotentialOutCome> largestPits, int[]the_pits, Outcome result) {
+
+
+        PotentialOutCome PotentialOutCome = null;
+        ArrayList<PotentialOutCome> potentialMoves = new ArrayList<PotentialOutCome>();
+        int potOut = 0;
+        //Set set = largestPits.entrySet();
+
+        outerloop:
+        for (int i = 0; i <  largestPits.size(); i++) {
+
+
+            int given_pit = largestPits.get(i).pitToMove;
+
+            if (given_pit > 31) {
+                System.out.println("null pointer exception!!!!!!");
+            }
+
+            int sumValue = largestPits.get(i).rocks;
+            //map enemy pit index to my corresponding pit index
+
+            //the given pit is enemy pit and you map to my pit (GAIN)
+            // if the given pit is my pit, then you map to the enemy pit (LOSS)
+            int mapping_pit = (the_pits.length - 1 - given_pit) + the_pits.length / 2;
+
+            //System.out.println("sumValue: " + sumValue+ " " + result);
+            //back track 10 pits and find the one that leads to capture this pit
+            innerloop:
+            for (int j = 2; j <= 11; j++) {
+                int tempPit = mapping_pit - j;
+
+
+                //if the number of rocks is equal to the number of moves required to that pit
+                if (the_pits[tempPit] == j && the_pits[mapping_pit] != 0) {
+
+                    //System.out.println("THERE IS A POSSIBLE CAPTURE");
+                    if (result == Outcome.GAIN) {
+                        //potential gain is the largest sum of that column
+                        potOut = sumValue;
+                        PotentialOutCome = new PotentialOutCome(tempPit, potOut);
+
+                        potentialMoves.add(PotentialOutCome);
+                        //break outerloop;
+                    } else if (result == Outcome.LOSS) {
+
+                        //System.out.println("Defend the potential loss: " + given_pit);
+                        potOut = sumValue;
+
+                        //move this pit if you want defend the potential loss
+                        PotentialOutCome = new PotentialOutCome(given_pit, potOut);
+                        potentialMoves.add(PotentialOutCome);
+
+                        break outerloop;
+                    }
+
+
+                }
+            }
+
+        }// end looping all the largest sum sets
+/*
+        if (result == Outcome.GAIN && queue.size() != 0) {
+            //if there are two similar pits one next to the other,
+            // favour the one in front
+            int pitToMove = queue.get(0).pitToMove;
+            int largestSum = queue.get(0).rocks;
+
+            for (int i = 0; i < queue.size(); i++){
+                if (largestSum == queue.get(i).rocks && queue.get(i).pitToMove > pitToMove) {
+                    return queue.get(i);
+                }
+            }
+
+            return queue.get(0);
+        }
+*/
+
+        //return the potential loss
+        return potentialMoves;
+
+    }
 
 
     public static int getMyTotalPit(int[] mypit) {
@@ -169,6 +259,9 @@ public class MyTools extends StudentPlayer{
 
         return 48 * 2;
     }
+
+
+
 
 
 
